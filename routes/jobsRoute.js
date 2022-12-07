9feddcd82606c72a465e7e6b0a9b4dfdf5a24cdf
saimzhd11 <saimzhd4@gmail.com>
2022-12-07 21:00:03 +0500

    Created more pages along with user model

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express=require("express");
 const router=express.Router();
 const Job = require("../models/jobModel");
@@ -10,4 +11,21 @@ router.get("/getalljobs",async(req,res)=>{
         return res.status(400).json({error})
     }
 });
+router.post("/postjob",async(req,res)=>{
+    try {
+        const newjob=new Job(req.body)
+        await newjob.save()
+        res.send('Job Posted Successfully')
+    } catch (error) {
+        return res.status(400).json({error});
+    }
+});
+router.post("/editjob",async(req,res)=>{
+    try {
+        const updatedjob=await Job.findOneAndUpdate({_id:req.body._id},req.body)
+        res.send('Job Updated Successfully')
+    } catch (error) {
+        return res.status(400).json({error});
+    }
+})
 module.exports=router;
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app=express();
 const db= require("./db.js");
 const jobsRoute=require('./routes/jobsRoute');
+const userRoute=require('./routes/usersRoute');
+app.use(express.json());
 app.use('/api/jobs/',jobsRoute)
-
+app.use('/api/users/',userRoute) 
 
 const port = process.env.PORT || 5000;
-app.listen(port,()=>console.log('Node js server started'));
+app.listen(port, ()=>console.log('Node js server started'));
